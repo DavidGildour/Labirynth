46efca19178974f5200ddb27e52c63cb80c2dd25
unknown <davidgildour@gmail.com>
2018-08-16 19:53:45 +0200

    it properly adds entrance and exit

--- cell.py
+++ cell.py
@@ -3,7 +3,7 @@ SIZE = int(input("Size: "))
 CORNER = '+'
 HOR_WALL = ' -- '
 VER_WALL = '| '
-PASSAGE = '  '
+PASSAGE = '    '
 
 class Cell:
     def __init__(self, i, j):
@@ -16,12 +16,12 @@ class Cell:
 
     def show(self):
         if cell.i + cell.j == 0: # the first cell at all
-            print((CORNER + HOR_WALL) * SIZE + CORNER)
+            print((CORNER + PASSAGE) + (CORNER + HOR_WALL) * (SIZE - 1) + CORNER)
         elif cell.i % SIZE == 0: # the first cell in a row (but not the first row)
             print(VER_WALL + '\n' + (CORNER + HOR_WALL) * SIZE + CORNER)
         print(VER_WALL + cell.row + cell.col, end = ' ')
         if cell.i + cell.j == (SIZE - 1) * 2: # the last cell
-            print(VER_WALL + '\n' + (CORNER + HOR_WALL) * SIZE + CORNER)
+            print(VER_WALL + '\n' + (CORNER + HOR_WALL) * (SIZE - 1) + (CORNER + PASSAGE) + CORNER)
 
 cells = []
 
