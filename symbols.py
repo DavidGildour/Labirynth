f5ecac5f2fba007c99d89e45a57f68eb8bcd96e9
unknown <davidgildour@gmail.com>
2018-08-16 19:42:44 +0200

    added a global variable to access anywhere

--- symbols.py
+++ symbols.py
@@ -1,5 +1,7 @@
 #IN ORDER TO ENNUMERATE GRIDS LARGER THAN 9 I NEEDED TO EXPAND THE DIGITS
 #'VOCABULARY'. SO NOW AFTER 9 THERE GOES ALL THE ALPHABET.
+SYMBOLS = '0123456789abcdefghijklmnopqrstuvwyz'
+
 def symbls():
     symbols = '0123456789abcdefghijklmnopqrstuvwyz'
     rows = []
